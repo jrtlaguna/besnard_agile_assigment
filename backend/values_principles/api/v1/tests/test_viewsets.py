3c36fc259e2506f3185fef91b3289b7208c72198
Ryan Laguna <jrtlaguna@gmail.com>
2021-09-27 16:41:10 +0800

    Updated tests into pytest

--- backend/values_principles/api/v1/tests/test_viewsets.py
+++ backend/values_principles/api/v1/tests/test_viewsets.py
@@ -1,118 +1,177 @@
-from rest_framework import status
-from rest_framework.test import APITestCase
-
-from django.urls import reverse
+import pytest
 
 from values_principles.models import Value, Principle
-from values_principles.tests.factories import ValueFactory, PrincipleFactory
 
 
-class ValuesViewSetTestCase(APITestCase):
-    def setUp(self):
-        self.value = ValueFactory()
-
-    def test_get_value(self):
-        res = self.client.get(reverse("values-principles-v1:values-list"))
-
-        self.assertEqual(res.data[0].get("id"), self.value.id)
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
-
-    def test_get_value_detail(self):
-        res = self.client.get(
-            reverse("values-principles-v1:values-detail", kwargs={"pk": self.value.id})
+@pytest.mark.django_db
+class TestFilmViews:
+    def test_create_value(self, client):
+        assert Value.objects.count() == 0
+        response = client.post(
+            "/api/v1/values_principles/values/",
+            {
+                "title": "Test Value Title",
+                "content": "Test Value Content",
+            },
         )
 
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
-
-    def test_create_value(self):
-        data = {
+        assert response.status_code == 201, response.data
+        assert response.json() == {
+            "id": response.json()["id"],
             "title": "Test Value Title",
             "content": "Test Value Content",
         }
 
-        value_count = Value.objects.count()
-        res = self.client.post(
-            reverse("values-principles-v1:values-list"), data=data, format="json"
-        )
-        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(value_count + 1, Value.objects.count())
-
-    def test_patch_value(self):
-        data = {"content": "New Content"}
-
-        res = self.client.patch(
-            reverse("values-principles-v1:values-detail", kwargs={"pk": 1}),
-            data=data,
-            format="json",
+    def test_list_value(self, client):
+        assert Value.objects.count() == 0
+        value = Value.objects.create(
+            title="Test Value Title",
+            content="Test Value Content",
         )
 
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.assertEqual(res.data.get("content"), "New Content")
+        response = client.get("/api/v1/values_principles/values/")
 
-    def test_delete_value(self):
-        res = self.client.delete(
-            reverse("values-principles-v1:values-detail", kwargs={"pk": 1})
+        assert response.status_code == 200
+        assert response.json() == [
+            {
+                "id": value.id,
+                "title": "Test Value Title",
+                "content": "Test Value Content",
+            }
+        ]
+
+    def test_get_value_detail(self, client):
+        assert Value.objects.count() == 0
+        value = Value.objects.create(
+            title="Test Value Title",
+            content="Test Value Content",
         )
-        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
+        response = client.get(f"/api/v1/values_principles/values/{value.id}/")
 
-        res = self.client.get(
-            reverse("values-principles-v1:values-detail", kwargs={"pk": 1})
+        assert response.status_code == 200
+        assert response.json() == {
+            "id": value.id,
+            "title": "Test Value Title",
+            "content": "Test Value Content",
+        }
+
+    def test_patch_value(self, client):
+        assert Value.objects.count() == 0
+        value = Value.objects.create(
+            title="Test Value Title",
+            content="Test Value Content",
         )
-        self.assertEqual(res.status_code, status.HTTP_404_NOT_FOUND)
-
-
-class PrinciplesViewSetTestCase(APITestCase):
-    def setUp(self):
-        self.principle = PrincipleFactory()
-
-    def test_get_value(self):
-        res = self.client.get(reverse("values-principles-v1:principles-list"))
-
-        self.assertEqual(res.data[0].get("id"), self.principle.id)
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
-
-    def test_get_principle_detail(self):
-        res = self.client.get(
-            reverse(
-                "values-principles-v1:principles-detail",
-                kwargs={"pk": self.principle.id},
-            )
+        response = client.patch(
+            f"/api/v1/values_principles/values/{value.id}/",
+            {"content": "New patched Value Content"},
+            content_type="application/json",
         )
 
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
+        assert response.status_code == 200
+        assert response.json() == {
+            "id": value.id,
+            "title": "Test Value Title",
+            "content": "New patched Value Content",
+        }
 
-    def test_create_principle(self):
-        data = {
+    def test_delete_value(self, client):
+        assert Value.objects.count() == 0
+        value = Value.objects.create(
+            title="Test Value Title",
+            content="Test Value Content",
+        )
+
+        response = client.delete(
+            f"/api/v1/values_principles/values/{value.id}/",
+        )
+        assert response.status_code == 204
+
+        response = client.get(
+            f"/api/v1/values_principles/values/{value.id}/",
+        )
+
+        assert response.status_code == 404
+
+
+@pytest.mark.django_db
+class TestFilmPrinciples:
+    def test_create_principle(self, client):
+        assert Principle.objects.count() == 0
+        response = client.post(
+            "/api/v1/values_principles/principles/",
+            {
+                "title": "Test Principle Title",
+                "content": "Test Principle Content",
+            },
+        )
+        assert response.status_code == 201, response.data
+
+    def test_list_principle(self, client):
+        assert Principle.objects.count() == 0
+        principle = Principle.objects.create(
+            title="Test Principle Title",
+            content="Test Principle Content",
+        )
+
+        response = client.get("/api/v1/values_principles/principles/")
+
+        assert response.status_code == 200
+        assert response.json() == [
+            {
+                "id": principle.id,
+                "title": "Test Principle Title",
+                "content": "Test Principle Content",
+            }
+        ]
+
+    def test_get_principle_detail(self, client):
+        assert Principle.objects.count() == 0
+        principle = Principle.objects.create(
+            title="Test Principle Title",
+            content="Test Principle Content",
+        )
+        response = client.get(f"/api/v1/values_principles/principles/{principle.id}/")
+
+        assert response.status_code == 200
+        assert response.json() == {
+            "id": principle.id,
             "title": "Test Principle Title",
             "content": "Test Principle Content",
         }
 
-        principle_count = Principle.objects.count()
-        res = self.client.post(
-            reverse("values-principles-v1:principles-list"), data=data, format="json"
+    def test_patch_principle(self, client):
+        assert Principle.objects.count() == 0
+        principle = Principle.objects.create(
+            title="Test Principle Title",
+            content="Test Principle Content",
         )
-        self.assertEqual(res.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(principle_count + 1, Principle.objects.count())
-
-    def test_patch_principle(self):
-        data = {"content": "New Content"}
-
-        res = self.client.patch(
-            reverse("values-principles-v1:principles-detail", kwargs={"pk": 1}),
-            data=data,
-            format="json",
+        response = client.patch(
+            f"/api/v1/values_principles/principles/{principle.id}/",
+            {"content": "New patched Principle Content"},
+            content_type="application/json",
         )
 
-        self.assertEqual(res.status_code, status.HTTP_200_OK)
-        self.assertEqual(res.data.get("content"), "New Content")
+        assert response.status_code == 200
+        assert response.json() == {
+            "id": principle.id,
+            "title": "Test Principle Title",
+            "content": "New patched Principle Content",
+        }
 
-    def test_delete_principle(self):
-        res = self.client.delete(
-            reverse("values-principles-v1:principles-detail", kwargs={"pk": 1})
+    def test_delete_principle(self, client):
+        assert Principle.objects.count() == 0
+        principle = Principle.objects.create(
+            title="Test Principle Title",
+            content="Test Principle Content",
         )
-        self.assertEqual(res.status_code, status.HTTP_204_NO_CONTENT)
 
-        res = self.client.get(
-            reverse("values-principles-v1:principles-detail", kwargs={"pk": 1})
+        response = client.delete(
+            f"/api/v1/values_principles/principles/{principle.id}/",
         )
-        self.assertEqual(res.status_code, status.HTTP_404_NOT_FOUND)
+        assert response.status_code == 204
+
+        response = client.get(
+            f"/api/v1/values_principles/principles/{principle.id}/",
+        )
+
+        assert response.status_code == 404
